2ada41d99cba03e1ef020a694e14e418a0f186a8
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-08-30 21:36:30 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+const date=new Date(2020,8,30,1);
+var hour=date.getHours();
+var msg="";
+var colors="";
+console.log(hour);
+if(hour>=0 && hour<12)
+{
+  console.log(1);
+  msg="Good Morning!";
+  colors="green";
+}
+else if(hour>=12 && hour<18)
+{
+    console.log(2);
+    msg="Good Afternoon!";
+    colors="red";
+}
+else
+{
+  console.log(3);
+  colors="blue"
+  msg="Good Night!";  
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<>
+<div className="greetingmsg">
+<span className="msg">{`Hello, `}</span>
+<span className="msg" id="showgreeting" style={{color:colors}}>{msg}</span>
+</div>
+</>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
